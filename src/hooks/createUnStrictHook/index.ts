ee08819d575725dcd54cf77d08feac73e8cf9377
wjjhhh <wujiajie@corp.yingzi.com>
2022-11-13 01:26:11 +0800

    feat: createStrictMemo init

--- src/hooks/createUnStrictHook/index.ts
+++ src/hooks/createUnStrictHook/index.ts
@@ -1,8 +1,39 @@
-import type { useEffect, useLayoutEffect } from 'react';
+import type { useEffect, useLayoutEffect, useMemo } from 'react';
 import { useRef } from 'react';
 
 type EffectHookType = typeof useEffect | typeof useLayoutEffect;
 type FuncType = (hook: EffectHookType) => EffectHookType;
+
+type AType = (hook:  typeof useMemo) =>  typeof useMemo
+
+let oldDeps: any = Symbol()
+
+const createStrictMemo: AType = (hook) => (effect, deps) =>  {
+    
+    return hook(() => {
+        
+        if (deps === null) {
+            deps = void 0
+        }
+        if (Array.isArray(oldDeps) && Array.isArray(deps) && oldDeps.length === deps.length) {
+            for(let i = 0; i < oldDeps.length; ++i) {
+                if (!Object.is(oldDeps[i], deps[i])) {
+                    oldDeps = deps
+                    return effect()
+                    
+                }
+            }
+        } else {
+            if (!Object.is(oldDeps, deps)) {
+                oldDeps = deps
+                return effect()
+            }
+            
+        }
+    }, [deps]) as ReturnType<typeof effect>
+    
+}
+
 
 const createUnStrictHook: FuncType = (hook) => (effect, deps) => {
   const isFirst = useRef(true);
