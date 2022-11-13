5a304c5b133ed2db24da270d3adde1ee51dcf276
wjjhhh <wujiajie@corp.yingzi.com>
2022-11-13 23:30:17 +0800

    refactor: createUnStrictHook

--- src/hooks/createUnStrictHook/index.ts
+++ src/hooks/createUnStrictHook/index.ts
@@ -4,50 +4,40 @@ import { useRef } from 'react';
 type EffectHookType = typeof useEffect | typeof useLayoutEffect;
 type FuncType = (hook: EffectHookType) => EffectHookType;
 
-type MemoHookType = (hook:  typeof useMemo) =>  typeof useMemo
+type MemoHookType = (hook: typeof useMemo) => typeof useMemo;
 
-let oldDeps: any = Symbol()
+let oldDeps: any = Symbol();
 
-const createStrictMemo: MemoHookType = (hook) => (effect, deps) =>  {
-    
-    return hook(() => {
-        
-        if (deps === null) {
-            deps = void 0
+const createStrictMemo: MemoHookType = (hook) => (effect, deps) => {
+  return hook(() => {
+    if (deps === null) {
+      deps = void 0;
+    }
+    if (Array.isArray(oldDeps) && Array.isArray(deps) && oldDeps.length === deps.length) {
+      for (let i = 0; i < oldDeps.length; ++i) {
+        if (!Object.is(oldDeps[i], deps[i])) {
+          oldDeps = deps;
+          return effect();
         }
-        if (Array.isArray(oldDeps) && Array.isArray(deps) && oldDeps.length === deps.length) {
-            for(let i = 0; i < oldDeps.length; ++i) {
-                if (!Object.is(oldDeps[i], deps[i])) {
-                    oldDeps = deps
-                    return effect()
-                    
-                }
-            }
-        } else {
-            if (!Object.is(oldDeps, deps)) {
-                oldDeps = deps
-                return effect()
-            }
-            
-        }
-    }, [deps]) as ReturnType<typeof effect>
-    
-}
-
+      }
+    } else {
+      if (!Object.is(oldDeps, deps)) {
+        oldDeps = deps;
+        return effect();
+      }
+    }
+  }, [deps]) as ReturnType<typeof effect>;
+};
 
 const createUnStrictHook: FuncType = (hook) => (effect, deps) => {
   const isFirst = useRef(true);
 
   hook(() => {
-    return () => {
+    if (isFirst.current && process.env.NODE_ENV === 'development') {
       isFirst.current = false;
-    };
-  }, []);
-
-  hook(() => {
-    if (isFirst.current) {
       return;
     }
+
     return effect();
   }, [deps]);
 };
