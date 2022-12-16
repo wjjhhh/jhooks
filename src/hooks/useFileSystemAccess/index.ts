aab161cfdd67a5fd6e75131c0fbb205870a483e4
wjjhhh <wujiajie@corp.yingzi.com>
2022-12-16 16:25:00 +0800

    type: useFileSystemAccess

--- src/hooks/useFileSystemAccess/index.ts
+++ src/hooks/useFileSystemAccess/index.ts
@@ -1,6 +1,6 @@
-// @ts-nocheck
+
 import { useState, useRef } from 'react';
-dataType: type Options = {
+type Options = {
   dataType?: 'Text' | 'ArrayBuffer' | 'Blob';
   types: {
     description?: string;
@@ -9,44 +9,60 @@ dataType: type Options = {
   multiple?: boolean;
 };
 
+interface FileHandleWritableStream extends WritableStream {
+    write: (data: string) => Promise<void>
+}
+type FileHandle = {
+    getFile: () => Promise<File>,
+    createWritable: () => Promise<FileHandleWritableStream>,
+}
 export default (options: Options) => {
-  const [file, setFile] = useState();
-  const [data, setData] = useState();
-  const handleRef = useRef();
+  const [file, setFile] = useState<File>();
+  const [data, setData] = useState<string>();
+  let handleRef = useRef<FileHandle>(null);
   const isSupported = 'showSaveFilePicker' in window && 'showOpenFilePicker' in window;
   const getFile = async () => {
-    const value = await handleRef.current.getFile();
+    const value = await handleRef.current?.getFile();
     setFile(value);
 
     if (options?.dataType === 'Text') {
-      const text = await value.text();
+      const text = await value?.text();
       updateData(text);
     }
   };
-  const updateData = (value) => {
+  const updateData = (value: any) => {
     setData(value);
   };
-  const open = async (_options) => {
+  const open = async (_options: Options) => {
+    if (!isSupported) {
+        return
+    }
     const [newFile] = await showOpenFilePicker({ ...options, ..._options });
 
     handleRef.current = newFile;
 
     getFile();
   };
-  const save = async (_options) => {
+  const save = async (_options: Options) => {
     if (!handleRef.current) {
       return saveAs(_options);
     }
 
     const writableStream = await handleRef.current.createWritable();
-    await writableStream.write(data);
+    await writableStream.write(data!);
     await writableStream.close();
   };
-  const saveAs = async (_options) => {
+  const saveAs = async (_options: Options) => {
+    if (!isSupported) {
+        return
+    }
     handleRef.current = await showSaveFilePicker({ ...options, ..._options });
-    const writableStream = await handleRef.current.createWritable();
-    await writableStream.write(data);
-    await writableStream.close();
+    const writableStream = await handleRef.current?.createWritable();
+    if (writableStream) {
+        await writableStream.write(data!);
+        await writableStream.close();
+    }
+   
   };
 
   const create = async () => {
