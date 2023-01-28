3c18d0991c4b049b277c7ea18ea0fe3f32585dbd
wjjhhh <wujiajie@corp.yingzi.com>
2023-01-28 14:32:32 +0800

    feat: useScroll init

---
+++ src/hooks/useScroll/index.ts
@@ -0,0 +1,54 @@
+import { useEffect, useSyncExternalStore, useCallback } from 'react';
+import { BasicTarget } from '../../types';
+import { getTargetElement } from '../../utils';
+
+type Position = {
+  top: number;
+  left: number;
+};
+
+let res: Position = {
+  top: 0,
+  left: 0,
+};
+
+function getSnapshot() {
+  return res;
+}
+
+const subscription = ([onChange, target]: [Function, BasicTarget]) => {
+  const _onChange = (v: BasicTarget) => {
+    res = {
+      top: v.target.scrollTop,
+      left: v.target.scrollLeft,
+    };
+    onChange(v, target);
+  };
+  const ele = getTargetElement(target);
+  ele.addEventListener('scroll', _onChange);
+  return () => {
+    ele.removeEventListener('scroll', _onChange);
+  };
+};
+
+function useScroll(target: BasicTarget, selector?: (val: Position) => any) {
+  useEffect(() => {
+    res = {
+      top: 0,
+      left: 0,
+    };
+  }, [target]);
+  const _subscription = useCallback(
+    (onChange: (e: Event) => void) => subscription([onChange, target]),
+    [target],
+  );
+  const _getSnapshot = useCallback(() => {
+    if (selector) {
+      return selector(getSnapshot());
+    }
+    return getSnapshot();
+  }, [target, selector]);
+  return useSyncExternalStore(_subscription, _getSnapshot);
+}
+
+export default useScroll;
