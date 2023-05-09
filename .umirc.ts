ef24941323f42950aaa9d88a65834970f4f03cd6
wujiajie <wujiajie@corp.yingzi.com>
2023-05-09 10:02:16 +0800

    docs: 换了个logo

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'jhooks',
   favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+    '/simple-logo.png',
+  logo: '/simple-logo.png',
   outputPath: 'docs-dist',
   mode: 'site',
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
