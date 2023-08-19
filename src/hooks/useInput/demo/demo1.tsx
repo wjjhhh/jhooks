78b9f0b70cbe023981bd310cecf3aed791e4dc1c
wujiajie <wujiajie@corp.yingzi.com>
2023-08-19 14:53:24 +0800

    feat: support useInput change active

--- src/hooks/useInput/demo/demo1.tsx
+++ src/hooks/useInput/demo/demo1.tsx
@@ -3,14 +3,18 @@ import React, { useState } from 'react';
 
 export default () => {
   const [content, setContent] = useState('');
+  const [active, setActive ] = useState(true)
   const props = useInput({
     onChange: setContent,
+    active,
   });
 
   return (
     <>
       <div>fetch所带参数：{content}</div>
       <input {...props} />
+      <div></div>
+      <button onClick={() => setActive(!active)}>{active ? '关闭' : '开启'}功能</button>
     </>
   );
 };
