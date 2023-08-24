2aebd2de934aa00e9b23ba6d86222198189ea2cd
wujiajie <wujiajie@corp.yingzi.com>
2023-08-24 16:58:26 +0800

    test: useBase64 init

--- src/hooks/useBase64/index.ts
+++ src/hooks/useBase64/index.ts
@@ -3,10 +3,14 @@ import { useEffect, useRef, useState } from 'react';
 export type UseBaseReturnType = {
   base64: string;
   isSupported: boolean;
+  promise: Promise<string>;
 };
+
+type Callback = (base64: string) => void;
 
 function toBase64(_target: Blob | string) {
   return new Promise<string>((resolve, reject) => {
+
     const reader = new FileReader();
 
     if (typeof _target === 'string') {
@@ -22,17 +26,25 @@ function toBase64(_target: Blob | string) {
   });
 }
 
-function useBase64(target: File): UseBaseReturnType;
-function useBase64(target: string): UseBaseReturnType;
-function useBase64(target: any) {
+function useBase64(target: File, callback?: Callback): UseBaseReturnType;
+function useBase64(target: string, callback?: Callback): UseBaseReturnType;
+function useBase64(target: any, callback?: Callback) {
   const [base64, setBase64] = useState('');
   const isSupported = useRef<boolean>();
+  const promise = useRef<Promise<string>>(new Promise(() => ''));
   if (isSupported.current === void 0) {
     isSupported.current = typeof FileReader !== void 0;
   }
   const exc = async (_target: Blob | string) => {
-    const res = await toBase64(_target);
-    setBase64(res);
+    // const res = await toBase64(_target);
+    // // callback?.(res)
+    // setBase64(res);
+    promise.current = new Promise((resolve, reject) => {
+      resolve(toBase64(_target));
+    });
+    promise.current.then((res) => {
+      setBase64(res);
+    });
   };
   useEffect(() => {
     exc(target);
@@ -40,6 +52,7 @@ function useBase64(target: any) {
   return {
     base64,
     isSupported: isSupported.current,
+    promise: promise.current,
   };
 }
 
