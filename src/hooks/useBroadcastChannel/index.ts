0064e5aa8276115301eef79a6953ad9eb44f7931
wjjhhh <378650926@qq.com>
2023-09-10 22:08:04 +0800

    feat: support useBroadcastChannel close (#70)
    Co-authored-by: wujiajie <wujiajie@corp.yingzi.com>

--- src/hooks/useBroadcastChannel/index.ts
+++ src/hooks/useBroadcastChannel/index.ts
@@ -2,32 +2,54 @@ import { useRef, useEffect, useState } from 'react';
 
 export default (name: string) => {
   const bcc = useRef<BroadcastChannel>();
-  const [data, setData] = useState()
-  const [error, setError] = useState<Event>()
+  const [data, setData] = useState();
+  const [error, setError] = useState<Event>();
+  const [isClosed, setIsClosed] = useState(false);
   if (!bcc.current) {
     bcc.current = new BroadcastChannel(name);
   }
-  const isSupported = 'BroadcastChannel' in window
+  const isSupported = 'BroadcastChannel' in window;
   useEffect(() => {
     if (bcc.current) {
-      bcc.current.addEventListener('message', (e: MessageEvent) => {
-        setData(e.data)
-      }, { passive: true });
+      bcc.current.addEventListener(
+        'message',
+        (e: MessageEvent) => {
+          setData(e.data);
+        },
+        { passive: true },
+      );
 
-      bcc.current.addEventListener('messageerror', (e: MessageEvent) => {
-        setError(e)
-      }, { passive: true });
-
+      bcc.current.addEventListener(
+        'messageerror',
+        (e: MessageEvent) => {
+          setError(e);
+        },
+        { passive: true },
+      );
+      // bcc.current.addEventListener('close', () => {
+      //   console.log('close')
+      // })
     }
+    return () => {};
   }, []);
 
   const post = (_data: unknown) => {
-    bcc.current?.postMessage(_data)   
-  }
+    if (isClosed) {
+      return
+    }
+    bcc.current?.postMessage(_data);
+  };
+  const close = () => {
+    bcc.current?.close();
+    setIsClosed(true);
+  };
   return {
     isSupported,
     data,
     error,
     post,
+    isClosed,
+    close,
+    channel: bcc.current,
   };
 };
