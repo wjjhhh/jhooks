a7134bad349e6731db68e621e6aeb84ab6df5fc3
wujiajie <wujiajie@corp.yingzi.com>
2023-11-01 09:48:24 +0800

    feat: useAnimations commitStyles

---
+++ src/hooks/useAnimations/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import { act, renderHook } from '@testing-library/react';
+import useAnimations from '..';
+
+describe('useAnimations', () => {
+    it('should be defined', () => {
+        expect(useAnimations).toBeDefined();
+    })
+})
