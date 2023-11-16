176125bc561a01b85305cbca24cf9f082e55e847
wujiajie <wujiajie@corp.yingzi.com>
2023-11-16 18:09:28 +0800

    test: useIdle lastActive and reset

--- src/hooks/useIdle/__tests__/index.test.tsx
+++ src/hooks/useIdle/__tests__/index.test.tsx
@@ -20,4 +20,32 @@ describe('useIdle', () => {
     fireEvent.mouseMove(element);
     expect(hook.result.current.isIdle).toBe(false);
   });
+  it('should reset timeout state', async () => {
+    const { getByTestId } = render(<div data-testid="test-element">Hello World!</div>);
+    const element = getByTestId('test-element');
+    const hook = renderHook(() => {
+      return useIdle(500);
+    });
+    fireEvent.touchStart(element);
+    await sleep(600);
+    hook.result.current.reset();
+    expect(hook.result.current.isIdle).toBe(true);
+    fireEvent.keyDown(element);
+    expect(hook.result.current.isIdle).toBe(false);
+    await sleep(600);
+    expect(hook.result.current.isIdle).toBe(true);
+  })
+  it('should update lastActive', async() => {
+    const { getByTestId } = render(<div data-testid="test-element">Hello World!</div>);
+    const element = getByTestId('test-element');
+    const hook = renderHook(() => {
+      return useIdle(500);
+    });
+    const lastActive0 = hook.result.current.lastActive
+    fireEvent.mouseDown(element);
+    const lastActive1 = hook.result.current.lastActive
+    expect(lastActive1).not.toBe(lastActive0)
+    await sleep(600);
+    expect(lastActive1).toBe(hook.result.current.lastActive)
+  })
 });
