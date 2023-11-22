d82e1a9cdaca1d3c4075bb2f341ba044d3e8a558
wujiajie <wujiajie@corp.yingzi.com>
2023-11-22 11:17:12 +0800

    fix: useCycleList none member

--- src/hooks/useCycleList/index.ts
+++ src/hooks/useCycleList/index.ts
@@ -5,19 +5,40 @@ function useCycleList<T>(list: T[]) {
   const [innerList, setInnerList] = useState<any[]>(list);
   const len = innerList.length;
   const prev = () => {
+    if (len < 2) {
+      return
+    }
     setIndex((old) => (old === 0 ? len - 1 : old - 1));
   };
   const next = () => {
+    if (len < 2) {
+      return
+    }
     setIndex((old) => (old + 1) % len);
   };
   const push = (member: any) => {
     setInnerList((prev) => [...prev, member]);
   };
   const pop = () => {
-    setInnerList((prev) => prev.slice(0, -1));
+    if (len === 0) {
+      return
+    }
+    if (index === len - 1) {
+      setIndex(index - 1)
+    }
+    const res = innerList.pop()
+    setInnerList([...innerList]);
+    
+    return res
   };
 
   const shift = () => {
+    if (len === 0) {
+      return
+    }
+    if (index === 0 && len === 1) {
+      setIndex(-1)
+    }
     const res = innerList.shift();
     setInnerList([...innerList]);
     return res;
