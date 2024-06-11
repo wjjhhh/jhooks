0f6b3ccc73a298940c25f32cd15dd789f85cb53a
wjjhhh <378650926@qq.com>
2024-06-11 09:21:51 +0800

    fix: useSignalUpdate unmount

--- src/hooks/useSignalUpdate/index1.tsx
+++ src/hooks/useSignalUpdate/index1.tsx
@@ -1,10 +1,11 @@
 import React, {  useEffect } from 'react';
-import { setEffectRun } from '../useSignal/index1';
+import { setEffectRun, resetEffectRun } from '../useSignal/index1';
 
 function useSignalUpdate(fn: () => void) {
   useEffect(() => {
     setEffectRun(fn);
     fn();
+    return resetEffectRun
   }, []);
 }
 
