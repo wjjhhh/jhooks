7a1474d9f9f306f427107fb1db31a58d298524bd
wjjhhh <378650926@qq.com>
2024-06-27 09:39:39 +0800

    docs: useSleep

--- src/hooks/useSleep/demo/demo1.tsx
+++ src/hooks/useSleep/demo/demo1.tsx
@@ -1,33 +1,27 @@
-import React from 'react'
-import { useSleep } from 'jhooks'
+import React from 'react';
+import { useSleep } from 'jhooks';
 
 export default () => {
-    const { sleep, destory } = useSleep(() => {
-        console.log('destory sleep success1!')
-    })
-    const { sleep: sleep2, destory: destory2 } = useSleep(() => {
-        console.log('destory sleep success2!')
-    })
-    const loogOp = async () => {
-        await sleep(3000)
-        console.log('loogOp:3s')
-        await sleep(2000)
-        console.log('loogOp:2s')
-    }
-    const loogOp2 = async () => {
-        await sleep2(3000)
-        console.log('loogOp2:3s')
-        await sleep2(2000)
-        console.log('loogOp2:2s')
-    }
-    return (
-        <>
-            <button onClick={loogOp}>耗时操作1</button>
-            <button onClick={destory}>暂停操作1</button>
-            <br />
-            <button onClick={loogOp2}>耗时操作2</button>
-            <button onClick={destory2}>暂停操作2</button>
-           
-        </>
-    )
-}
+  const [status, setStatus] = React.useState('idle');
+  const { sleep, destory } = useSleep(() => {
+    setStatus('destructive');
+  });
+
+  const loogOp = async () => {
+    setStatus('working');
+    await sleep(3000);
+    setStatus('working after 3000ms');
+    await sleep(2000);
+    setStatus('finish');
+  };
+
+  return (
+    <>
+      status: {status}
+      <br />
+      <button onClick={loogOp}>耗时操作1</button>
+      <br />
+      <button onClick={destory}>暂停操作1</button>
+    </>
+  );
+};
--- src/hooks/useSleep/index.md
+++ src/hooks/useSleep/index.md
@@ -7,6 +7,28 @@ group:
 ---
 
 # useSleep
+提供延时和打断能力的hook
 
+## 代码演示
 
+### 基础用法
 <code src="./demo/demo1.tsx" />
+
+## API
+
+```typescript
+
+const { sleep, destory } = useSleep(fn?);
+```
+
+### Params
+
+| 参数 | 说明 | 类型 | 默认值 |
+| --- | --- | --- | --- |
+| fn | 打断的回调函数 | () => void | - |
+
+### Result
+| 参数 | 说明 | 类型 |
+| --- | --- | --- |
+| sleep | 延时函数 | (time: number) => void |
+| destory | 打断函数 | () => void |
