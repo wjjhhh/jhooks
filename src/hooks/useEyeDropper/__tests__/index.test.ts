384aace6e245050c8c9b900af4a9e97514dd34ac
wjjhhh <378650926@qq.com>
2024-08-26 09:08:22 +0800

    test: useEyeDropper

---
+++ src/hooks/useEyeDropper/__tests__/index.test.ts
@@ -0,0 +1,72 @@
+import { renderHook, act, waitFor } from '@testing-library/react';
+import useEyeDropper from '..';
+
+declare global {
+  interface Window {
+    EyeDropper: any;
+  }
+}
+
+describe('useEyeDropper', () => {
+  it('should initialize with default values ', () => {
+    window.EyeDropper = jest.fn();
+    const { result } = renderHook(() => useEyeDropper());
+    expect(result.current.color).toBeNull();
+    expect(result.current.isSupported).toBe(true);
+    delete window.EyeDropper;
+  });
+
+  it('should set the color when EyeDropper is opened successfully', async () => {
+    const mockOpen = jest.fn().mockResolvedValue({ sRGBHex: '#ffffff' });
+    window.EyeDropper = jest.fn().mockImplementation(() => ({
+      open: mockOpen,
+    }));
+    const { result } = renderHook(() => useEyeDropper());
+    await act(async () => {
+      result.current.openEyeDropper();
+    });
+
+    expect(mockOpen).toHaveBeenCalled();
+    expect(result.current.color).toBe('#ffffff');
+  });
+
+  it('should log an error when EyeDropper is not supported', () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    delete window.EyeDropper;
+
+    const { result } = renderHook(() => useEyeDropper());
+    act(() => {
+      result.current.openEyeDropper();
+    });
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      'EyeDropper API is not supported in your browser.',
+    );
+    expect(result.current.color).toBeNull();
+
+    consoleErrorSpy.mockRestore();
+  });
+
+  it('should log an error when EyeDropper fails to open', async () => {
+    const mockOpen = jest.fn().mockRejectedValue(new Error('Failed to open EyeDropper'));
+    window.EyeDropper = jest.fn().mockImplementation(() => ({
+      open: mockOpen,
+    }));
+
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const { result } = renderHook(() => useEyeDropper());
+    await act(async () => {
+      result.current.openEyeDropper();
+    });
+
+    expect(mockOpen).toHaveBeenCalled();
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      'EyeDropper failed:',
+      new Error('Failed to open EyeDropper'),
+    );
+    expect(result.current.color).toBeNull();
+
+    consoleErrorSpy.mockRestore();
+  });
+});
