633501827e80b80419294727d5348f7e2a8246f2
wjjhhh <378650926@qq.com>
2024-09-22 15:13:17 +0800

    feat: useWakeLock support onError

--- src/hooks/useWakeLock/__tests__/index.test.ts
+++ src/hooks/useWakeLock/__tests__/index.test.ts
@@ -4,7 +4,8 @@ import useWakeLock from '..';
 describe('useWakeLock', () => {
   const onLock = jest.fn();
   const onRelease = jest.fn();
-  const options = { onLock, onRelease };
+  const onError = jest.fn();
+  const options = { onLock, onRelease, onError };
 
   beforeAll(() => {
     Object.defineProperty(navigator, 'wakeLock', {
@@ -12,8 +13,9 @@ describe('useWakeLock', () => {
       value: {
         request: jest.fn().mockResolvedValue({
           addEventListener: jest.fn((event, handler) => {
+            console.log('event', event);
             if (event === 'release') {
-              handler();
+              setTimeout(handler, 1000);
             }
           }),
           release: jest.fn().mockResolvedValue(undefined),
@@ -22,59 +24,76 @@ describe('useWakeLock', () => {
     });
   });
 
-  //   it('should initialize with default values', () => {
-  //     const { result } = renderHook(() => useWakeLock(options));
-  //     expect(result.current.isSupported).toBe(true);
-  //     expect(result.current.wakeLock).toBeNull();
-  //   });
+  it('should initialize with default values', async () => {
+    const { result } = renderHook(() => useWakeLock(options));
+    expect(result.current.isSupported).toBe(true);
 
-  //   it('should acquire wake lock and call onLock', async () => {
-  //     const { result } = renderHook(() => useWakeLock(options));
-
-  //     await act(async () => {
-  //       await result.current.lock();
-  //     });
-
-  //     expect(navigator.wakeLock.request).toHaveBeenCalledWith('screen');
-  //     expect(result.current.wakeLock).not.toBeNull();
-  //     expect(onLock).toHaveBeenCalled();
-  //   });
-
-  // it('should release wake lock and call onRelease', async () => {
-  //   const { result } = renderHook(() => useWakeLock(options));
-
-  //   await act(async () => {
-  //     await result.current.lock();
-  //     await result.current.release();
-  //   });
-
-  //   expect(result.current.wakeLock).toBeNull();
-  //   expect(onRelease).toHaveBeenCalled();
-  // });
-
-    it('should re-acquire wake lock on visibility change', async () => {
-      const { result } = renderHook(() => useWakeLock(options));
-
-      expect(navigator.wakeLock.request).toHaveBeenCalledTimes(1);
-
-      await act(async () => {
-        await result.current.lock();
-      });
-
-      expect(navigator.wakeLock.request).toHaveBeenCalledTimes(2);
-
-      act(() => {
-        Object.defineProperty(document, 'visibilitychange', {
-          value: 'hidden',
-          configurable: true,
-        })
-        document.dispatchEvent(new Event('visibilitychange'));
-      });
-
-      await act(async () => {
-        await result.current.lock();
-      });
-
-      expect(navigator.wakeLock.request).toHaveBeenCalledTimes(3);
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve));
     });
+    expect(result.current.wakeLock).not.toBeNull();
+  });
+
+  it('should acquire wake lock and call onLock', async () => {
+    const { result } = renderHook(() => useWakeLock(options));
+
+    await act(async () => {
+      await result.current.lock();
+    });
+
+    expect(navigator.wakeLock.request).toHaveBeenCalledWith('screen');
+
+    expect(result.current.wakeLock).not.toBeNull();
+
+    expect(onLock).toHaveBeenCalled();
+  });
+
+  it('should release wake lock and call onRelease', async () => {
+    const { result } = renderHook(() => useWakeLock(options));
+
+    await act(async () => {
+      await result.current.lock();
+      await result.current.release();
+    });
+    await new Promise((resolve) => setTimeout(resolve, 1100));
+    expect(result.current.wakeLock).toBeNull();
+    expect(onRelease).toHaveBeenCalled();
+  });
+  it('should call onError when lock fails', async () => {
+    (global.navigator.wakeLock.request as jest.Mock).mockRejectedValue(new Error('Lock failed'));
+
+    const { result } = renderHook(() => useWakeLock(options));
+
+    await act(async () => {
+      await result.current.lock();
+    });
+
+    expect(options.onError).toHaveBeenCalledWith(expect.any(Error));
+  });
+
+  it('should re-acquire wake lock on visibility change', async () => {
+    const { result } = renderHook(() => useWakeLock(options));
+
+    expect(navigator.wakeLock.request).toHaveBeenCalledTimes(1);
+
+    await act(async () => {
+      await result.current.lock();
+    });
+
+    expect(navigator.wakeLock.request).toHaveBeenCalledTimes(2);
+
+    act(() => {
+      Object.defineProperty(document, 'visibilitychange', {
+        value: 'hidden',
+        configurable: true,
+      });
+      document.dispatchEvent(new Event('visibilitychange'));
+    });
+
+    await act(async () => {
+      await result.current.lock();
+    });
+
+    expect(navigator.wakeLock.request).toHaveBeenCalledTimes(3);
+  });
 });
